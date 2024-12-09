19ea20d45ec6b6c376a1000d712e190b1c593c1e
Sarvesh-Changan <changansarvesh0@gmail.com>
2024-12-09 14:02:29 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,34 +4,39 @@ import { motion } from "framer-motion";
 const Contact = () => {
   return (
     <div className="border-b border-neutral-900 pb-20">
-        <motion.h1 
+      <motion.h1
         whileInView={{ opacity: 1, y: 0 }}
-        initial={{ opacity: 0, y: -100}}
+        initial={{ opacity: 0, y: -100 }}
         transition={{ duration: 0.5 }}
-        className="my-10 text-center text-4xl">
-          Get in Touch
-        </motion.h1>
-        <div className="text-center tracking-tighter">
-            <motion.p 
+        className="my-10 text-center text-4xl"
+      >
+        Get in Touch
+      </motion.h1>
+      <div className="text-center tracking-tighter">
+        <motion.p
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 1 }}
+          className="my-4"
+        >
+          {CONTACT.address}
+        </motion.p>
+        <a href="tel:+918104610045" className="my-4">
+          <motion.p
             whileInView={{ opacity: 1, x: 0 }}
-            initial={{ opacity: 0, x: -100}}
+            initial={{ opacity: 0, x: 100 }}
             transition={{ duration: 1 }}
-            className="my-4">
-              {CONTACT.address}
-            </motion.p>
-            <motion.p 
-            whileInView={{ opacity: 1, x: 0 }}
-            initial={{ opacity: 0, x: 100}}
-            transition={{ duration: 1 }}
-            className="my-4">
-              {CONTACT.phoneNo}
-            </motion.p>
-            <a href="mailto:changansarvesh0@gmail.com" className="border-b">
-          {CONTACT.email}
-            </a>
-        </div>
-    </div>
-  )
-}
+          >
+            {CONTACT.phoneNo}
+          </motion.p>
+        </a>
 
-export default Contact
+        <a href="mailto:changansarvesh0@gmail.com" className="border-b">
+          {CONTACT.email}
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
